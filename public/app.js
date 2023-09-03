2e3f91f92bedfa25fff64b8f1c4d1b0a7f8cd5bb
Iamneo <techadmin@iamneo.ai>
2023-09-03 11:40:04 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,59 +1,79 @@
-const cells = document.querySelectorAll(".cell");
-const resultText = document.getElementById("winner-text");
-const resetButton = document.getElementById("reset-button");
-const resultDiv = document.getElementById("result");
+// Initial game state
+let cells = ['', '', '', '', '', '', '', '', ''];
+let currentPlayer = 'X';
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-let currentPlayer = "X";
-let board = ["", "", "", "", "", "", "", "", ""];
+// Function to handle player moves
+const ticTacToe = (element, index) => {
+    // Your game logic here
 
-// Check for a win or a draw
-function checkWinner() {
-    const winningCombinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],
-        [0, 4, 8], [2, 4, 6]
-    ];
-
-    for (const combination of winningCombinations) {
-        const [a, b, c] = combination;
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            resultText.textContent = `${currentPlayer} wins!`;
-            resultDiv.style.display = "block";
-            return;
+    const ticTacToe = (element, index) => {
+        // Check if the selected cell is empty
+        if (cells[index] === '') {
+            cells[index] = currentPlayer;
+            element.textContent = currentPlayer;
+            
+            // Check for winning conditions
+            for (const condition of conditions) {
+                const [a, b, c] = condition;
+                if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+                    result.textContent = `${currentPlayer} wins!`;
+                    btns.forEach(btn => btn.disabled = true); // Disable all buttons
+                    return; // Exit the function
+                }
+            }
+            
+            // Switch to the next player
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            
+            // Update the current player's turn in the 'result' element
+            result.textContent = `Current Player: ${currentPlayer}`;
         }
-    }
+    };
+};
 
-    if (!board.includes("")) {
-        resultText.textContent = "It's a draw!";
-        resultDiv.style.display = "block";
-    }
-}
+    /*
+    **Part 2: Reset Function (Add your code here)**
 
-// Handle cell click
-function handleCellClick(e) {
-    const cell = e.target;
-    const cellIndex = cell.getAttribute("id").split("-")[1];
+    1. Implement a new function that resets the game to its initial state.
+    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
+    3. Update the 'result' element to indicate the current player's turn.
+    4. Re-enable all buttons for a new game.
+    */
 
-    if (board[cellIndex] === "" && !resultDiv.style.display) {
-        board[cellIndex] = currentPlayer;
-        cell.textContent = currentPlayer;
-        cell.classList.add(currentPlayer);
-        checkWinner();
+// Function to reset the game
+const resetGame = () => {
+    const resetGame = () => {
+        // Reset the game state
+        cells = ['', '', '', '', '', '', '', '', ''];
+        currentPlayer = 'X';
+        
+        // Clear the content of all cells and enable buttons
+        btns.forEach((btn, i) => {
+            btn.textContent = '';
+            btn.disabled = false;
+        });
+        
+        // Update the 'result' element
+        result.textContent = `Current Player: ${currentPlayer}`;
+    };
+    
+    document.querySelector('#reset').addEventListener('click', resetGame);
+};
 
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-    }
-}
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => ticTacToe(btn, i));
+});
 
-// Handle reset button click
-function handleResetClick() {
-    board = ["", "", "", "", "", "", "", "", ""];
-    cells.forEach(cell => {
-        cell.textContent = "";
-        cell.classList.remove("X", "O");
-    });
-    resultDiv.style.display = "none";
-    currentPlayer = "X";
-}
-
-cells.forEach(cell => cell.addEventListener("click", handleCellClick));
-resetButton.addEventListener("click", handleResetClick);
+document.querySelector('#reset').addEventListener('click', resetGame);
